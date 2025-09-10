46b3df7a17b2b59b599b355b79d3ace69c697410
Aditya Singh <singhaditya0020@gmail.com>
2025-09-10 17:28:14 +0530

    Router update

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Navbar from './Navbar';
 import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Body from './Body';
+import Login from './Login';
+import Profile from './Profile';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,7 +13,10 @@ function App() {
 
     <BrowserRouter basename='/'>
     <Routes>
-      <Route path="/" element={<Body/>}  />
+      <Route path="/" element={<Body/>}>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/profile' element={<Profile/>}/>
+      </Route>  
     </Routes>
     </BrowserRouter>
 
--- src/Body.jsx
+++ src/Body.jsx
@@ -6,6 +6,7 @@ const Body = () => {
   return (
     <div>
       <Navbar/>
+      <Outlet/>
     </div>
   )
 }
