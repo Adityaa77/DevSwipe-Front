bd3aba1ba3aed593b4bc065b77aac6ff19c77a8a
Aditya Singh <singhaditya0020@gmail.com>
2025-09-10 17:19:13 +0530

    navbar and react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import { useState } from 'react';
 import Navbar from './Navbar';
+import { BrowserRouter, Routes,Route } from 'react-router-dom';
+import Body from './Body';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
+
+    <BrowserRouter basename='/'>
+    <Routes>
+      <Route path="/" element={<Body/>}  />
+    </Routes>
+    </BrowserRouter>
+
+
+
     <Navbar/>
  
     </>
---
+++ src/Body.jsx
@@ -0,0 +1,13 @@
+import Navbar   from "./Navbar";
+
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default Body
