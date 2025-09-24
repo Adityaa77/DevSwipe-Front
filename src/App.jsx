39a15176ea77fbad65e3c44257b8e4705e59f6c6
Aditya Singh <singhaditya0020@gmail.com>
2025-09-25 02:26:58 +0530

    making  the redux store

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter, Routes,Route } from 'react-router-dom';
 import Body from './Body';
 import Login from './Login';
 import Profile from './Profile';
+import { Provider } from 'react-redux';
+import appStore from './utls/reduxstore';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-
+   <Provider store={appStore}>{/*This is for the redux store */}
     <BrowserRouter basename='/'>
     <Routes>
       <Route path="/" element={<Body/>}>
@@ -19,7 +21,7 @@ function App() {
       </Route>  
     </Routes>
     </BrowserRouter>
- 
+   </Provider>
     </>
   );
 }
