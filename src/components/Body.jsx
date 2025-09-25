2d785c2f42e75cb8d2c4560f2071d526604ab254
Aditya Singh <singhaditya0020@gmail.com>
2025-09-26 03:15:50 +0530

    functioning navbar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,37 @@
 import Footer from "./Footer";
 import Navbar   from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import React from 'react'
+import { useDispatch, useSelector } from "react-redux";
+import {BASE_URL} from "../utls/constants";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Body = () => {
+  const dispatch=useDispatch();
+  const navigate=useNavigate();
+  const userData=useSelector((store)=>store.user);
+
+  const fetchUser=async()=>{
+    if(userData) return;
+    try{
+      const res=await axios.get(BASE_URL+"/profile/view",{
+        withCredentials: true,
+      });
+      dispatch(addUser(res.data));
+    }catch(arr){
+      if(err.staus==401){
+      navigate('/login');
+      }
+      console.error(err);
+    }
+  };
+
+  useEffect(()=>{
+    
+     fetchUser();
+    
+  },[])
   return (
     <div>
       <Navbar/>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
-
+import { useDispatch, useSelector } from 'react-redux'
+import {Link} from "react-router-dom";
+import {removeUser} from "../utls/userslice";
 const Navbar = () => {
   const user=useSelector(store=>store.user);
+  const dispatch=useDispatch();
+  const navigate=userNavigate();
 
+  const handleLogout = async()=>{
+    try{
+  await axios.post(BASE_URL+"/logout",{},{withCredentials: true});
+   dispatch(removeUser());
+   return Navigate("/login")
+  }catch(err){
+      
+    }
+  }
 
   return (
      <div className="navbar bg-base-100 shadow-sm">
   <div className="flex-1">
-    <a className="btn btn-ghost text-xl">DevSwipe</a>
+    <Link to="/" className="btn btn-ghost text-xl">DevSwipe</Link>
   </div>
   <div className="flex gap-2">
     <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto" />
@@ -24,10 +36,10 @@ const Navbar = () => {
         tabIndex={0}
         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
         <li>
-          <a className="justify-between">
+          <Link to="/profile" className="justify-between">
             Profile
             <span className="badge">New</span>
-          </a>
+          </Link>
         </li>
         <li><a>Settings</a></li>
         <li><a>Logout</a></li>
