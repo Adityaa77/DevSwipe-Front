0624a9e3e90c86e1fd0dee83e927ec87e82cb6db
Aditya Singh <singhaditya0020@gmail.com>
2025-09-29 05:03:35 +0530

    fix

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -12,32 +12,29 @@ const Feed = () => {
   const getFeed = async () => {
     if (feed) return;
     try {
-      const res = await axios.get(BASE_URL+"/feed", {
+      const res = await axios.get(BASE_URL + "/feed", {
         withCredentials: true,
       });
       dispatch(addFeed(res?.data?.data));
-      
     } catch (err) {
-      // TODO: handle error
-      console.error("Feed error:", err.response?.data || err.message);
+      //TODO: handle error
     }
   };
 
   useEffect(() => {
     getFeed();
-    // eslint-disable-next-line
   }, []);
-
-  if (!feed) return null;
+  if (!feed) return;
 
   if (feed.length <= 0)
-    return <h1 className="flex justify-center my-10">No new users found!</h1>;
+    return <h1 className="flex justify-center my-10">No new users founds!</h1>;
 
   return (
-    <div className="flex justify-center my-10">
-      <UserCard user={feed[0]} />
-    </div>
+    feed && (
+      <div className="flex justify-center my-10">
+        <UserCard user={feed[0]} />
+      </div>
+    )
   );
 };
-
 export default Feed;
--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,48 +1,50 @@
-import axios from 'axios';
-import React from 'react'
-import { BASE_URL } from '../utls/constants';
-import { useDispatch } from 'react-redux';
-import { removeUserFromFeed } from '../utls/feedslice';
+import axios from "axios";
+import { BASE_URL } from "../utls/constants";
+import { useDispatch } from "react-redux";
+import { removeUserFromFeed } from "../utls/feedslice";
 
-const UserCard = ({user}) => {
-  const{_id, Name,LastName,Age,Gender,PhotoUrl,About}=user;
-  const dispatch=useDispatch();
+const UserCard = ({ user }) => {
+  const { _id, Name, LastName, PhotoUrl, Age, Gender, About } = user;
+  const dispatch = useDispatch();
 
-  const handleSendRequest=async(status,userId)=>{
-    try{
-    const res=await axios.post(
-      BASE_URL + "/request/send/" + status +"/" + userId, {},
-      {withCredentials:true}
-    );
-    dispatch(removeUserFromFeed(userId));
-    }catch(err){
-
+  const handleSendRequest = async (status, userId) => {
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/send/" + status + "/" + userId,
+        {},
+        { withCredentials: true }
+      );
+      dispatch(removeUserFromFeed(userId));
+    } catch (err) {
+       dispatch(removeUserFromFeed(userId));
     }
-  }
-
+  };
 
   return (
-    <div className="card bg-base-300 w-96 shadow-sm">
-  <figure className='my-2'>
-    <img
-      src={user.PhotoUrl}
-      alt="Profile Photo" />
-  </figure>
-  <div className="card-body">
-    <h2 className="card-title">{Name +" "+LastName}</h2>
-    <p>{Age +" "+Gender}</p>
-    <p>{About}</p>
-    <div className="card-actions justify-center my-4">
-      <button 
-      className="btn btn-primary"
-      onClick={()=>handleSendRequest("ignored",_id)}>Ignore</button>
-      <button 
-      className="btn btn-primary"
-      onClick={()=>handleSendRequest("interested",_id)}>Interested</button>
+    <div className="card bg-base-300 w-96 shadow-xl">
+      <figure>
+        <img src={user.PhotoUrl} alt="photo" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">{Name + " " + LastName}</h2>
+        {Age && Gender && <p>{Age + ", " + Gender}</p>}
+        <p>{About}</p>
+        <div className="card-actions justify-center my-4">
+          <button
+            className="btn btn-primary"
+            onClick={() => handleSendRequest("ignored", _id)}
+          >
+            Ignore
+          </button>
+          <button
+            className="btn btn-secondary"
+            onClick={() => handleSendRequest("interested", _id)}
+          >
+            Interested
+          </button>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-  )
-}
-
-export default UserCard
+  );
+};
+export default UserCard;
