c9c9bec6414431f17bea57f06b41374dc02329f8
Aditya Singh <singhaditya0020@gmail.com>
2025-10-14 01:55:53 +0530

    fixed the connection requestion frontend

--- src/components/ConnectionRequests.jsx
+++ src/components/ConnectionRequests.jsx
@@ -2,36 +2,36 @@ import axios from "axios";
 import { BASE_URL } from "../utls/constants";
 import { useDispatch, useSelector } from "react-redux";
 import { addRequests, removeRequest } from "../utls/requestslice";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const ConnectionRequests = () => {
   const requests = useSelector((store) => store.requests);
   const dispatch = useDispatch();
 
   const reviewRequest = async (status, _id) => {
-  try {
-    const res = await axios.post(
-      BASE_URL + "/request/review/" + status + "/" + _id,
-      {},
-      { withCredentials: true }
-    );
-    console.log(res.data.message);
-    dispatch(removeRequest(_id));
-  } catch (err) {
-    console.error("Error reviewing request:", err.response?.data || err.message);
-  }
-};
-
-
+    try {
+      const res = await axios.post(
+        BASE_URL + "/request/review/" + status + "/" + _id,
+        {},
+        { withCredentials: true }
+      );
+      console.log(res.data.message);
+      dispatch(removeRequest(_id));
+    } catch (err) {
+      console.error("Error reviewing request:", err.response?.data || err.message);
+    }
+  };
 
   const fetchRequests = async () => {
     try {
       const res = await axios.get(BASE_URL + "/user/requests/received", {
         withCredentials: true,
       });
-
+     console.log("Fetched requests:", res.data.data);
       dispatch(addRequests(res.data.data));
-    } catch (err) {}
+    } catch (err) {
+      console.error("Error fetching requests:", err.response?.data || err.message);
+    }
   };
 
   useEffect(() => {
@@ -48,26 +48,21 @@ const ConnectionRequests = () => {
       <h1 className="text-bold text-white text-3xl">Connection Requests</h1>
 
       {requests.map((request) => {
-        if (!request.fromUserId) return null;
-           const { _id, Name, LastName, PhotoUrl, Age, Gender, About } = request.fromUserId;
+       const user = request.fromuserId || request.fromUserId;
+              if (!user) return null;
+              const { _id, Name, LastName, PhotoUrl, Age, Gender, About } = user;
 
 
         return (
           <div
             key={_id}
-            className=" flex justify-between items-center m-4 p-4 rounded-lg bg-base-300  mx-auto"
+            className="flex justify-between items-center m-4 p-4 rounded-lg bg-base-300 mx-auto"
           >
             <div>
-              <img
-                alt="photo"
-                className="w-20 h-20 rounded-full"
-                src={PhotoUrl}
-              />
+              <img alt="photo" className="w-20 h-20 rounded-full" src={PhotoUrl} />
             </div>
-            <div className="text-left mx-4 ">
-              <h2 className="font-bold text-xl">
-                {Name + " " + LastName}
-              </h2>
+            <div className="text-left mx-4">
+              <h2 className="font-bold text-xl">{Name + " " + LastName}</h2>
               {Age && Gender && <p>{Age + ", " + Gender}</p>}
               <p>{About}</p>
             </div>
@@ -91,4 +86,5 @@ const ConnectionRequests = () => {
     </div>
   );
 };
+
 export default ConnectionRequests;
--- src/components/Connections.jsx
+++ src/components/Connections.jsx
@@ -3,57 +3,65 @@ import { BASE_URL } from '../utls/constants'
 import axios from 'axios'
 import { useDispatch, useSelector } from 'react-redux'
 import { addConnections } from '../utls/connectionSlice'
+import { Link } from "react-router-dom";
 
 const Connections = () => {
-    const connections=useSelector(store=>store.connections);
-    const dispatch=useDispatch();
-    const fetchconnections=async()=>{
-        try{
-            const res=await axios.get(BASE_URL+"/user/connections",{
-                withCredentials:true,
+    const connections = useSelector(store => store.connections);
+    const dispatch = useDispatch();
+
+    const fetchconnections = async () => {
+        try {
+            const res = await axios.get(BASE_URL + "/user/connections", {
+                withCredentials: true,
             });
+            console.log("Fetched connections from API:", res.data.data); // ✅ added here
             dispatch(addConnections(res.data.data));
-        }catch(err){
+        } catch (err) {
 
         }
     }
 
-    useEffect(()=>{
-       fetchconnections();
-    },[])
-    if(!connections) return;
-    if(connections.length===0)return<h1>No Connections Found</h1>
+    useEffect(() => {
+        fetchconnections();
+    }, [])
 
-  return (
-    <div className='text center my-10'>
-      <h1 className='text-bold text-2xl'>Connections</h1>
-         {connections.map((connection) => {
-        const { _id, Name, LastName, PhotoUrl, Age, Gender, About } =
-          connection;
+    console.log("Connections from Redux:", connections); // ✅ added here
 
-        return (
-          <div
-            key={_id}
-            className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
-          >
-            <div>
-              <img
-                alt="photo"
-                className="w-20 h-20 rounded-full object-cover"
-                src={PhotoUrl}
-              />
-            </div>
-            <div className="text-left mx-4 ">
-              <h2 className="font-bold text-xl">
-                {Name + " " + LastName}
-              </h2>
-              {Age && Gender && <p>{Age + ", " + Gender}</p>}
-              <p>{About}</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+    if (!connections) return;
+    if (connections.length === 0) return <h1>No Connections Found</h1>
+
+    return (
+        <div className='text center my-10'>
+            <h1 className='text-bold text-2xl'>Connections</h1>
+            {connections.map((connection) => {
+                console.log("Each connection object:", connection); // ✅ added here
+                const { _id, Name, LastName, PhotoUrl, Age, Gender, About } =
+                    connection;
+
+                return (
+                    <div
+                        key={_id}
+                        className="flex m-4 p-4 rounded-lg bg-base-300 w-1/2 mx-auto"
+                    >
+                        <div>
+                            <img
+                                alt="photo"
+                                className="w-20 h-20 rounded-full object-cover"
+                                src={PhotoUrl}
+                            />
+                        </div>
+                        <div className="text-left mx-4 ">
+                            <h2 className="font-bold text-xl">
+                                {Name + " " + LastName}
+                            </h2>
+                            {Age && Gender && <p>{Age + ", " + Gender}</p>}
+                            <p>{About}</p>
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 };
+
 export default Connections;
